9c2d244fc993e42889c1d032ae41ce2894f35060
Vũ Quang Hào <108588018+Hao-1337@users.noreply.github.com>
2024-07-27 10:00:49 +0700

    Delete lib/src directory

--- lib/src/refined-storage/path-finder.d.ts
+++
@@ -1,8 +0,0 @@
-import { Vector3 } from '@minecraft/server';
-export type PathFinderGroup = (PathFinderGroup | Vector3[])[];
-export declare class PathFinder {
-    paths: Vector3[];
-    group: PathFinderGroup;
-    contructor(): void;
-    protected init(): void;
-}
